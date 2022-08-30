e9920ddd52246a186b05a8ed3e713331913ec58d
difoss <difoss@163.com>
2022-08-30 10:38:04 +0800

    :bug: mulit user supported
    install.sh will NOT write ~/.bashrc instand by /etc/profile.d/easy-sh-alias.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,16 @@ if [ $DIFOSS_ENV_BASE ];then
     exit 0
 fi
 
-# append content of `bashrc_increment.template` to `~/.bashrc`,
-# and replace current path instead of string "{{REALPATH_OF_PWD}}"
-cat bashrc_increment.template >> ~/.bashrc
-sed -i "s#{{REALPATH_OF_PWD}}#$PWD#g" ~/.bashrc
-source ~/.bashrc
-echo easy-alias-sh install done !
+tmp_file=/etc/profile.d/easy-sh-alias.sh
 
+# append content of `bashrc_increment.template` to `/etc/profile.d`,
+# and replace current path instead of string "{{REALPATH_OF_PWD}}"
+if [ -f "$tmp_file" ]; then
+    echo "easy-sh-alias had already installed on <$DIFOSS_ENV_BASE>."
+	exit 0
+else
+    cat bashrc_increment.template >> "$tmp_file"
+    sed -i "s#{{REALPATH_OF_PWD}}#$PWD#g" "$tmp_file"
+    source "$tmp_file"
+    echo easy-alias-sh install done !
+fi
