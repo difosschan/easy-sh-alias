e01a098920529a07cf536e3bf24836af0f71d4b3
chenruirong <chenruirong@ncti-gba.cn>
2022-11-23 14:55:49 +0800

    :bug: fix non-root user install problem

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # check whether $DIFOSS_ENV_BASE environment variable
-if [ $DIFOSS_ENV_BASE ];then
-    echo 'easy-sh-alias seems to be installed ($DIFOSS_ENV_BASE exists).'
+if [[ ! -z "$DIFOSS_ENV_BASE" ]]; then
+    echo "easy-sh-alias seems to be installed oon <$DIFOSS_ENV_BASE>."
     exit 0
 fi
 
@@ -10,12 +10,21 @@ tmp_file=/etc/profile.d/easy-sh-alias.sh
 
 # append content of `bashrc_increment.template` to `/etc/profile.d`,
 # and replace current path instead of string "{{REALPATH_OF_PWD}}"
-if [ -f "$tmp_file" ]; then
-    echo "easy-sh-alias had already installed on <$DIFOSS_ENV_BASE>."
-	exit 0
-else
+if [ ! -f "$tmp_file" ]; then
     cat bashrc_increment.template >> "$tmp_file"
     sed -i "s#{{REALPATH_OF_PWD}}#$PWD#g" "$tmp_file"
-    source "$tmp_file"
-    echo easy-alias-sh install done !
 fi
+
+if [[ x"$USER" = 'xroot' ]]; then
+    source "$tmp_file"
+else
+    IFS=$'\n'
+    for line in `cat $tmp_file`; do
+        echo $line >> ~/.bashrc
+    done
+
+    source ~/.bashrc
+fi
+
+echo easy-alias-sh install done !
+
